a2f9fb61fed87226843762fd6796963c5ec53113
yudialfayat <ayudi3521@gmail.com>
2022-05-05 00:13:05 +0700

    Basic Styling

--- src/components/chat-room.svelte
+++ src/components/chat-room.svelte
@@ -45,18 +45,16 @@
 <div class="chat-room">
 	<p>Room Id: {roomId}</p>
 	<div class="paste-container">
-		<ul>
-			{#if pastes.length !== 0}
-				{#each pastes as paste}
-					<li>
-						<div>{moment.unix(paste.createAt).format('DD-MM-YYYY HH:mm:ss')}</div>
-						<div>{paste.text}</div>
-					</li>
-				{/each}
-			{:else}
-				<p>There is no data</p>
-			{/if}
-		</ul>
+		{#if pastes.length !== 0}
+			{#each pastes as paste}
+				<div class="paste">
+					<div class="date">{moment.unix(paste.createAt).format('DD-MM-YYYY HH:mm:ss')}</div>
+					<div class="text">{paste.text}</div>
+				</div>
+			{/each}
+		{:else}
+			<p>There is no data</p>
+		{/if}
 	</div>
 	<div class="form-container">
 		<form on:submit|preventDefault={() => sendPaste()}>
@@ -67,33 +65,67 @@
 </div>
 
 <style>
+	#dummy {
+		background-color: red;
+		width: 100px;
+		height: 100px;
+	}
 	.chat-room {
-		width: 300px;
-		height: 450px;
+		width: 400px;
+		height: 600px;
 		background-color: white;
 		margin-top: 50px;
 		color: black;
+		display: flex;
+		flex-direction: column;
 	}
 
 	.form-container {
 		width: 100%;
-		padding: 2px;
+		padding: 10px 20px;
 		background-color: gray;
 	}
 	.form-container form {
 		display: flex;
+		gap: 10px;
 	}
 	.form-container input {
+		border: none;
 		width: 100%;
+		padding: 5px 10px;
+		border-radius: 10px;
+	}
+
+	button {
+		padding: 5px 8px;
 	}
 
 	.paste-container {
 		background-color: rgb(236, 236, 236);
 		height: 100%;
 		text-align: end;
+		display: flex;
+		flex-direction: column;
+		gap: 5px;
+		overflow-y: scroll;
+		padding-bottom: 10px;
+		padding-top: 10px;
 	}
 
-	ul {
-		list-style: none;
+	.paste {
+		margin-right: 5px;
+		margin-left: 5px;
+		border-radius: 10px;
+		padding: 5px 10px;
+		box-shadow: 2px 3px rgba(0, 0, 0, 0.2);
+	}
+
+	.paste .date {
+		text-align: start;
+		font-size: x-small;
+	}
+
+	.paste .text {
+		font-size: small;
 	}
 </style>
