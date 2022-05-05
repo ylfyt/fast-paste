c4c0f889d4ab298c01fcbe03d1a7ec8bad6328c0
yudialfayat <ayudi3521@gmail.com>
2022-05-05 22:52:13 +0700

    Icon Button

--- src/components/input.svelte
+++ src/components/input.svelte
@@ -3,12 +3,15 @@
 	import { nanoid } from 'nanoid';
 	import { db } from '../utils/firebase';
 	import type { IPaste } from '../utils/interfaces';
+	import SendButton from './send-button.svelte';
 
 	export let roomId: string;
 
 	let text = '';
+	let loading = false;
 	const sendPaste = async () => {
 		if (text === '') return;
+		loading = true;
 		try {
 			const newPaste: IPaste = {
 				createAt: Math.round(Date.now() / 1000),
@@ -23,13 +26,14 @@
 		} catch (error) {
 			console.log(error);
 		}
+		loading = false;
 	};
 </script>
 
 <div class="form-container">
 	<form on:submit|preventDefault={() => sendPaste()}>
 		<textarea type="text" bind:value={text} placeholder="Text" />
-		<button type="submit">send</button>
+		<SendButton type="submit" disabled={text == '' || loading} isBusy={loading} backgroundColor="#F5DEB3" />
 	</form>
 </div>
 
@@ -40,6 +44,7 @@
 	}
 	.form-container form {
 		display: flex;
+		align-items: flex-end;
 		gap: 10px;
 	}
 	.form-container textarea {
---
+++ src/components/send-button.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import LoadingIcon from './svg-icons/loading-icon.svelte';
+	import SendIcon from './svg-icons/send-icon.svelte';
+
+	export let backgroundColor: string;
+	export let type: string;
+	export let isBusy: boolean;
+	export let disabled: boolean;
+</script>
+
+<button {type} disabled={isBusy || disabled} style={`background-color: ${backgroundColor};`}>
+	{#if isBusy}
+		<LoadingIcon />
+	{:else}
+		<SendIcon />
+	{/if}
+</button>
+
+<style>
+	button {
+		padding: 5px 20px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 10px;
+		border: none;
+		box-shadow: 2px 4px 20px rgba(0, 0, 0, 0.2);
+	}
+
+	button:hover {
+		color: aqua;
+		cursor: pointer;
+	}
+
+	button:enabled:active {
+		box-shadow: none;
+	}
+
+	button:disabled {
+		cursor: default;
+	}
+</style>
