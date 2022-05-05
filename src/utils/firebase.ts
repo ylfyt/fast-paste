c6108ae25e278c7385348bc6bddec321bacd6f43
yudialfayat <ayudi3521@gmail.com>
2022-05-05 15:18:46 +0700

    change app to easypaste

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, where, query } from 'firebase/firestore';
-import { collectionData } from 'rxfire/firestore';
-import { tap } from 'rxjs/operators';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyCHzUASBf-gcLLug4pkrfQfTlazHM2jWEE',
-	authDomain: 'coba-firebase-9a008.firebaseapp.com',
-	projectId: 'coba-firebase-9a008',
-	storageBucket: 'coba-firebase-9a008.appspot.com',
-	messagingSenderId: '1035839586311',
-	appId: '1:1035839586311:web:5cab4452178659cce32aa2',
+	apiKey: 'AIzaSyCbTkw5RwYqlUzWSZAVcpV0bDTGTSxYIm4',
+	authDomain: 'easy-paste.firebaseapp.com',
+	projectId: 'easy-paste',
+	storageBucket: 'easy-paste.appspot.com',
+	messagingSenderId: '887256609466',
+	appId: '1:887256609466:web:7f6ca07a2e80af496dc883',
 };
 
 const app = initializeApp(firebaseConfig);
