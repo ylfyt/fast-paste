edf5b0f2e0bdbc2b4d18151d08b1841d1f440828
ylfyt <ayudi3521@gmail.com>
2023-02-08 17:21:18 +0700

    Adding Google Analytics

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyCbTkw5RwYqlUzWSZAVcpV0bDTGTSxYIm4',
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
+getAnalytics(app);
